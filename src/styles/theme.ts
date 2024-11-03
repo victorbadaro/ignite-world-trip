3e68e3fdae9892b5bc019439af42eb5312b60eee
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-03 15:53:09 -0300

    feat: add global styles and a new font

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,14 +2,25 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
 	colors: {
-		highlight: {
+		yellow: {
 			500: '#FFBA08'
+		},
+		gray: {
+			100: '#F5F8FA', // Light/Headings and Text
+			200: '#DADADA', // Light/Info
+			700: '#999999', // Dark/Info
+			800: '#47585B' // Dark/Headings and Text
 		}
+	},
+	fonts: {
+		heading: 'Poppins',
+		body: 'Poppins'
 	},
 	styles: {
 		global: {
 			body: {
-				bg: 'highlight.500'
+				bg: 'gray.100',
+				color: 'gray.800'
 			}
 		}
 	}
