201bcb52496d19c1d0307895b4f3b8bb8dd334a9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-03 12:25:07 -0300

    feat: add _document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { theme } from '../styles/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<>
+			<Head>
+				<title>worldtrip</title>
+			</Head>
+
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</>
 	);
 }
