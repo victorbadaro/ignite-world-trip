ce80cc79e962bd395e8ca0746ab833c15d7c0279
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-12 18:09:35 -0300

    refactor: switch from miragejs to json-server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,9 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Header } from '../components/header';
-import { makeServer } from '../services/mirage';
 import { theme } from '../styles/theme';
 
 import '../styles/slider.css';
-
-makeServer();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
