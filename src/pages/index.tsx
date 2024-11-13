9f3e8aae5b73ba7e3a92f988c9f5a13666f346fe
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-13 17:54:58 -0300

    feat: improve slider comeponent for smaller screens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -144,11 +144,11 @@ export default function Home({ continents }: InferGetStaticPropsType<typeof getS
 								<a>
 									<Image w="100%" h="100%" objectFit="cover" src={continent.imageUrl} alt={continent.name} />
 									<Flex position="absolute" top="0" left="0" w="100%" h="100%" align="center" justify="center" bgColor="rgb(28, 20, 1, 0.35)">
-										<VStack spacing="4">
-											<Heading fontSize="5xl" fontWeight="bold" color="gray.100">
+										<VStack spacing={['3', '4']}>
+											<Heading fontSize={['2xl', '5xl']} fontWeight="bold" color="gray.100">
 												{continent.name}
 											</Heading>
-											<Text fontSize="2xl" fontWeight="bold" color="gray.200">
+											<Text fontSize={['sm', '2xl']} fontWeight="bold" color="gray.200">
 												{continent.description}
 											</Text>
 										</VStack>
