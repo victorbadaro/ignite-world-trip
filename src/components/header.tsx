0576b0abf99aaee4726abff2bdea6d87b5301894
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-13 17:06:57 -0300

    feat: improve header component for smaller screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,27 @@
-import { Flex, Link as ChakraLink } from '@chakra-ui/react';
+import { Flex, Image, Link as ChakraLink, useBreakpointValue } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FiChevronLeft } from 'react-icons/fi';
 
 export function Header() {
 	const { asPath } = useRouter();
+	const isSmallScreen = useBreakpointValue({
+		base: true,
+		md: false
+	});
 
 	return (
-		<Flex align="center" h="100px" bg="gray.100">
+		<Flex align="center" h={['50px', '100px']} bgColor="gray.100">
 			<Flex flex="1" position="relative" align="center" justify="center" mx="auto" maxW="1160px">
 				{asPath !== '/' && (
 					<Link href="/" passHref>
 						<ChakraLink as="a" position="absolute" left="0" color="gray.800">
-							<FiChevronLeft size={32} />
+							<FiChevronLeft size={isSmallScreen ? 16 : 32} />
 						</ChakraLink>
 					</Link>
 				)}
 
-				<img src="/images/Logo.svg" alt="WorldTrip Logo" />
+				<Image src="/images/Logo.svg" alt="WorldTrip Logo" w={['81px', 'auto']} h={['20px', 'auto']} />
 			</Flex>
 		</Flex>
 	);
